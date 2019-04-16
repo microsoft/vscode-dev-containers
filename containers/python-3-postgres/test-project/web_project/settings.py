ac5b865711de4136146203f86dc662386e9ae970
Dan Taylor (VS) <dantaylo@microsoft.com>
2019-04-15 23:56:26 -0700

    Move django project to generic python3+postgres project

--- containers/python-3-postgres/test-project/web_project/settings.py
+++ containers/python-3-postgres/test-project/web_project/settings.py
@@ -61,11 +61,14 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'LocalPassword',
+        'HOST': 'db',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
