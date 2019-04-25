c4e231373939c75574d4d1e13b6e4c8b86e91d5c
Chuck Lantz <clantz@microsoft.com>
2019-04-25 14:06:21 -0700

    Make LICENSE a link so it can be clicked when used in projects

--- containers/dart/test-project/server.dart
+++ containers/dart/test-project/server.dart
@@ -1,3 +1,8 @@
+/*--------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
 import 'dart:io';
 
 Future main() async {
--- containers/python-3-postgres/test-project/manage.py
+++ containers/python-3-postgres/test-project/manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#-----------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
 
 import os
 import sys
