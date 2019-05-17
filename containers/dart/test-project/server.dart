d9ae89cf7deacb0ee82e6b363ca1e06c0a95ea4b
Danny Tuppeny <danny@tuppeny.com>
2019-05-17 11:06:55 +0100

    Dart: fix pedantic lint for unawaited future

--- containers/dart/test-project/server.dart
+++ containers/dart/test-project/server.dart
@@ -4,14 +4,14 @@
  *-------------------------------------------------------------------------------------------------------------*/
 
 import 'dart:io';
+import 'package:pedantic/pedantic.dart';
 
 Future main() async {
-
-
-  var server = await HttpServer.bind(
+  final server = await HttpServer.bind(
     InternetAddress.loopbackIPv4,
     8080,
   );
+  
   print('*************************************************************************');
   print('* Press F1, select "Remote-Containers: Forward Port from Container...", *');
   print('* and select the server port listed below to access server.             *');
@@ -19,8 +19,7 @@ Future main() async {
   print('Listening on localhost:${server.port}');
 
   await for (HttpRequest request in server) {
-    request.response
-      ..write('Hello remote world!')
-      ..close();
+    request.response.write('Hello remote world!');
+    unawaited(request.response.close());
   }
 }
