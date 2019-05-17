bbf316667f8912bced2ed619dd0d2a860b1c46c1
Danny Tuppeny <danny@tuppeny.com>
2019-05-17 11:35:16 +0100

    Dart: Remove unnecessary instructions + format

--- containers/dart/test-project/bin/server.dart
+++ containers/dart/test-project/bin/server.dart
@@ -10,14 +10,12 @@ Future main() async {
     InternetAddress.loopbackIPv4,
     8080,
   );
-  
-  print('*************************************************************************');
-  print('* Press F1, select "Remote-Containers: Forward Port from Container...", *');
-  print('* and select the server port listed below to access server.             *');
-  print('*************************************************************************');
+
   print('Listening on localhost:${server.port}');
 
   await for (HttpRequest request in server) {
-    request.response..write('Hello remote world!')..close();
+    request.response
+      ..write('Hello remote world!')
+      ..close();
   }
 }
