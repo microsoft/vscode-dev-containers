21ee06fb189434fcbb0df8160d3097033dd4b617
Phillip Carter <pcarter@fastmail.com>
2019-11-22 08:59:59 -0800

    Add dotnetcore-3.0-fsharp

---
+++ containers/dotnetcore-3.0-fsharp/test-project/Program.fs
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+// ----------------------------------------------------------------------------------------
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let from = "F# Container"
+    let target = "Remote World"
+    let message = "Hello " + target + " from the " + from + "!"
+    printfn "%s" message
+    
+    0 // return an integer exit code
