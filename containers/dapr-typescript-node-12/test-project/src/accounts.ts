b87be710be553bb872295238ef4d2a2b23897d59
Phillip Hoff <phillip@orst.edu>
2020-01-09 16:14:14 -0800

    Update sample to use Dapr.

--- containers/dapr-typescript-node-12/test-project/src/accounts.ts
+++ containers/dapr-typescript-node-12/test-project/src/accounts.ts
@@ -1,17 +1,40 @@
 import * as express from 'express';
+import DaprClient from './daprClient';
 
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
-    res.send(req.params.id);
+const daprClient = new DaprClient();
+
+router.use(express.json({ strict: false }));
+
+router.get('/:id', async (req, res) => {
+    const balance = await daprClient.getState<number>(req.params.id);
+
+    if (balance !== undefined) {
+        res.status(200).header('Content-Type', 'application/json').send(JSON.stringify(balance));
+    } else {
+        res.sendStatus(404);
+    }
 });
 
-router.post('/:id/deposit', (req, res) => {
-    res.send(req.params.id);
+router.post('/:id/deposit', async (req, res) => {
+    let balance = await daprClient.getState<number>(req.params.id) ?? 0;
+
+    balance += req.body as number;
+
+    await daprClient.setState(req.params.id, balance);
+
+    res.status(200).header('Content-Type', 'application/json').send(JSON.stringify(balance));
 });
 
-router.post('/:id/withdraw', (req, res) => {
-    res.send(req.params.id);
+router.post('/:id/withdraw', async (req, res) => {
+    let balance = await daprClient.getState<number>(req.params.id) ?? 0;
+
+    balance -= req.body as number;
+
+    await daprClient.setState(req.params.id, balance);
+
+    res.status(200).header('Content-Type', 'application/json').send(JSON.stringify(balance));
 });
 
 export default router;
