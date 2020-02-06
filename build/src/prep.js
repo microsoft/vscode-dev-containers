d264e38efef6bdb1ef90f5094ccb0d579f0819ee
Chuck Lantz <clantz@microsoft.com>
2020-02-05 19:33:18 -0800

    Command to update script versions

--- build/src/prep.js
+++ build/src/prep.js
@@ -27,7 +27,7 @@ async function prepDockerFile(devContainerDockerfilePath, definitionId, repo, re
     const devContainerDockerfileRaw = await asyncUtils.readFile(devContainerDockerfilePath);
     
     // Replace script URL and generate SHA if applicable
-    let devContainerDockerfileModified = await lockScriptVersion(devContainerDockerfileRaw, repo, release, true);
+    let devContainerDockerfileModified = await updateScriptUrl(devContainerDockerfileRaw, repo, release, true);
 
     if (isForBuild) {
         // If building, update FROM to target registry and version if definition has a parent
@@ -105,8 +105,8 @@ async function updateConfigForRelease(definitionPath, definitionId, repo, releas
 }
 
 // Replace script URL and generate SHA if applicable
-async function lockScriptVersion(devContainerDockerfileRaw, repo, release, lockSha) {
-    lockSha = typeof lockSha === 'undefined' ? true : lockSha;
+async function updateScriptUrl(devContainerDockerfileRaw, repo, release, updateScriptSha) {
+    updateScriptSha = typeof updateScriptSha === 'undefined' ? true : updateScriptSha;
     
     // Replace script URL and generate SHA if applicable
     const scriptCaptureGroups = new RegExp(`COMMON_SCRIPT_SOURCE="(.+)/${scriptLibraryPathInRepo.replace('.', '\\.')}/(.+)"`).exec(devContainerDockerfileRaw);
@@ -114,23 +114,34 @@ async function lockScriptVersion(devContainerDockerfileRaw, repo, release, lockS
         const scriptName = scriptCaptureGroups[2];
         const scriptSource = `https://raw.githubusercontent.com/${repo}/${release}/${scriptLibraryPathInRepo}/${scriptName}`;
         let sha = scriptSHA[scriptName];
-        if (lockSha && typeof sha === 'undefined') {
+        if (updateScriptSha && typeof sha === 'undefined') {
             const scriptRaw = await asyncUtils.getUrlAsString(scriptSource);
             sha = await asyncUtils.shaForString(scriptRaw);
             scriptSHA[scriptName] = sha;
         }
         return devContainerDockerfileRaw
-            .replace(/COMMON_SCRIPT_SHA=".+"/, `COMMON_SCRIPT_SHA="${lockSha ? sha : 'dev-mode'}"`)
+            .replace(/COMMON_SCRIPT_SHA=".+"/, `COMMON_SCRIPT_SHA="${updateScriptSha ? sha : 'dev-mode'}"`)
             .replace(/COMMON_SCRIPT_SOURCE=".+"/, `COMMON_SCRIPT_SOURCE="${scriptSource}"`);
 
     }
     return devContainerDockerfileRaw;
 }
 
-async function lockScriptVersionInDockerfile(devContainerDockerfilePath, repo, release, lockSha) {
+// Update script URL in a Dockerfile to be release specific (or not) and optionally update the SHA to lock to this version
+async function updateScriptUrlInDockerfile(devContainerDockerfilePath, repo, release, updateScriptSha) {
     const devContainerDockerfileRaw = await asyncUtils.readFile(devContainerDockerfilePath);
-    const devContainerDockerfileModified = await lockScriptVersion(devContainerDockerfileRaw, repo, release, lockSha);
+    const devContainerDockerfileModified = await updateScriptUrl(devContainerDockerfileRaw, repo, release, updateScriptSha);
     await asyncUtils.writeFile(devContainerDockerfilePath, devContainerDockerfileModified);
+}
+
+// Update all script URLS in the entire repo (not staging folder)
+async function updateAllScriptUrlsInRepo(repo, release, updateScriptSha) {
+    const definitionFolder = path.join(__dirname, '..', '..', 'containers');
+    // Update script versions in definition Dockerfiles for release
+    const allDefinitions = await asyncUtils.readdir(definitionFolder);
+    await asyncUtils.forEach(allDefinitions, async (currentDefinitionId) => {
+        await updateScriptUrlInDockerfile(path.join(definitionFolder, currentDefinitionId), repo, release, updateScriptSha);
+    });
 }
 
 module.exports = {
@@ -138,5 +149,6 @@ module.exports = {
     updateStub: updateStub,
     updateConfigForRelease: updateConfigForRelease,
     prepDockerFile: prepDockerFile,
-    lockScriptVersionInDockerfile: lockScriptVersionInDockerfile
+    updateScriptUrlInDockerfile: updateScriptUrlInDockerfile,
+    updateAllScriptUrlsInRepo: updateAllScriptUrlsInRepo
 }
