fe119e1cda1158089bbec1312cf757c42c320509
Chuck Lantz <clantz@microsoft.com>
2020-02-07 10:08:29 -0800

    Update patch tool to support image ids

--- build/src/patch.js
+++ build/src/patch.js
@@ -12,36 +12,65 @@ async function patch(patchPath, registry, registryPath) {
     patchPath = path.resolve(patchPath);
     const patchConfigFilePath = path.resolve(patchPath, 'patch.json');
 
-
     const patchConfig = require(patchConfigFilePath);
     if (typeof patchConfig.bumpVersion === 'undefined') {
         patchConfig.bumpVersion = true;
     }
 
+    const tagList = patchConfig.tagList || []
+    // If imageIds specified, get the complete list of tags to update
+
+    if (patchConfig.imageIds) {
+        // ACR registry name is the registry minus .azurecr.io
+        const registryName = registry.replace('.azurecr.io', '');
+        // Get list of repositories
+        console.log(`(*) Getting repository list for ACR "${registryName}"...`)
+        const repositoryListOutput = await asyncUtils.spawn('az',
+            ['acr', 'repository', 'list', '--name', registryName],
+            { shell: true, stdio: 'pipe' });
+        const repositoryList = JSON.parse(repositoryListOutput);
+
+        // Query each repository for images, then add any tags found to the list
+        const query =  patchConfig.imageIds.reduce((prev, current) => {
+                    return prev ?  `${prev} || digest=='${current}'` : `"[?digest=='${current}'`;
+           }, null) + '].tags | []"';
+        await asyncUtils.forEach(repositoryList, async (repository) => {
+            console.log(`(*) Searching for tags in "${repository}"...`)
+            const registryTagListOutput = await asyncUtils.spawn('az',
+                ['acr', 'repository', 'show-manifests', '--name', registryName, '--repository', repository, "--query", query],
+                { shell: true, stdio: 'pipe' });
+            const registryTagList = JSON.parse(registryTagListOutput);
+            registryTagList.forEach((tag) => {
+                tagList.push(`${repository}:${tag}`);
+            });
+        });
+    }
+    console.log(`(*) Tags to patch: ${JSON.stringify(tagList, 4)}`);
+
     const spawnOpts = { stdio: 'inherit', cwd: patchPath,  shell: true };
-    await asyncUtils.forEach(patchConfig.tagList, async (tag) => {
-        const fullTag = `${registry}/${registryPath}/${tag}`;
-        const fullNewTag = `${registry}/${registryPath}/${patchConfig.bumpVersion ? updateVersionTag(tag) : tag}`;
-        console.log(`\n(*) Patching ${fullTag}...`);
+    await asyncUtils.forEach(tagList, async (tag) => {
+        const newTag = patchConfig.bumpVersion ? updateVersionTag(tag, registryPath) : tag;
+        console.log(`\n(*) Patching ${tag}...`);
 
         // Pull and build patched tag
         await asyncUtils.spawn('docker', [
                 'build',
                 '--pull',
-                '--build-arg', `ORIGINAL_IMAGE=${fullTag}`,
-                '--tag', fullNewTag,
-                '-f', patchConfig.dockerFile || 'Dockerfile',
+                '--build-arg', `ORIGINAL_IMAGE=${registry}/${tag}`,
+                '--tag', `${registry}/${newTag}`,
+                '-f', `${patchPath}/${patchConfig.dockerFile || 'Dockerfile'}`,
                 patchPath
             ], spawnOpts);
 
         // Push update
-        await asyncUtils.spawn('docker', ['push', fullNewTag], spawnOpts);
+        await asyncUtils.spawn('docker', ['push', `${registry}/${newTag}`], spawnOpts);
     });
 
     console.log('\n(*) Done!')
 }
 
-function updateVersionTag(tag) {
+function updateVersionTag(fullTag, registryPath) {
+    const tag = fullTag.replace(registryPath + '/', '');
     // Get the version number section of the tag if it exists
     const colon = tag.indexOf(':');
     const firstDash = tag.indexOf('-', colon);
@@ -53,7 +82,7 @@ function updateVersionTag(tag) {
             // If there are, update the break fix version
             return `${tag.substring(0, colon + 1)}${versionParts[0]}.${versionParts[1]}.${versionParts[2] + 1}${tag.substring(firstDash)}`;
         }
-        return tag;
+        return `${registryPath}:${tag}`;
     }
     return tag;
 }
