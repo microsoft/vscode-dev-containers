d71b51f6dd2497ba68856921406d2caf4b8653b4
Chuck Lantz <clantz@microsoft.com>
2020-04-02 10:12:33 -0700

    Improve patch utility

--- build/src/patch.js
+++ build/src/patch.js
@@ -5,66 +5,50 @@
 
 const path = require('path');
 const asyncUtils = require('./utils/async');
+const jsonc = require('jsonc').jsonc;
 
 async function patch(patchPath, registry, registryPath) {
+    patchPath = path.resolve(patchPath);
+    const patchConfig = await getPatchConfig(patchPath);
+    // ACR registry name is the registry minus .azurecr.io
+    const registryName = registry.replace(/\..*/, '');
 
     console.log(`(*) Applying patch located at "${patchPath}"...`);
-    patchPath = path.resolve(patchPath);
-    const patchConfigFilePath = path.resolve(patchPath, 'patch.json');
-
-    const patchConfig = require(patchConfigFilePath);
-    if (typeof patchConfig.bumpVersion === 'undefined') {
-        patchConfig.bumpVersion = true;
-    }
-
-    const tagList = patchConfig.tagList || []
+    let tagList = patchConfig.tagList || [];
     // If imageIds specified, get the complete list of tags to update
-
     if (patchConfig.imageIds) {
-        // ACR registry name is the registry minus .azurecr.io
-        const registryName = registry.replace(/\..*/, '');
-        // Get list of repositories
-        console.log(`(*) Getting repository list for ACR "${registryName}"...`)
-        const repositoryListOutput = await asyncUtils.spawn('az',
-            ['acr', 'repository', 'list', '--name', registryName],
-            { shell: true, stdio: 'pipe' });
-        const repositoryList = JSON.parse(repositoryListOutput);
-
-        // Query each repository for images, then add any tags found to the list
-        const query =  patchConfig.imageIds.reduce((prev, current) => {
-                    return prev ?  `${prev} || digest=='${current}'` : `"[?digest=='${current}'`;
-           }, null) + '].tags | []"';
-        await asyncUtils.forEach(repositoryList, async (repository) => {
-            console.log(`(*) Searching for tags in "${repository}"...`)
-            const registryTagListOutput = await asyncUtils.spawn('az',
-                ['acr', 'repository', 'show-manifests', '--name', registryName, '--repository', repository, "--query", query],
-                { shell: true, stdio: 'pipe' });
-            const registryTagList = JSON.parse(registryTagListOutput);
-            registryTagList.forEach((tag) => {
-                tagList.push(`${repository}:${tag}`);
-            });
-        });
+        const manifests = await getImageManifests(registryName, patchConfig.imageIds);
+        tagList = tagList.concat(manifests.reduce((prev, manifest) => {
+            return prev.concat(manifest.tags.map((tag) => {
+                return `${manifest.repository}:${tag}`;
+            }));
+        }, []));
     }
-    console.log(`(*) Tags to patch: ${JSON.stringify(tagList, 4)}`);
+    console.log(`(*) Tags to patch: ${JSON.stringify(tagList, undefined, 4)}`);
 
-    const spawnOpts = { stdio: 'inherit', cwd: patchPath,  shell: true };
+    const spawnOpts = { stdio: 'inherit', cwd: patchPath, shell: true };
     await asyncUtils.forEach(tagList, async (tag) => {
         const newTag = patchConfig.bumpVersion ? updateVersionTag(tag, registryPath) : tag;
         console.log(`\n(*) Patching ${tag}...`);
 
         // Pull and build patched tag
         await asyncUtils.spawn('docker', [
-                'build',
-                '--pull',
-                '--build-arg', `ORIGINAL_IMAGE=${registry}/${tag}`,
-                '--tag', `${registry}/${newTag}`,
-                '-f', `${patchPath}/${patchConfig.dockerFile || 'Dockerfile'}`,
-                patchPath
-            ], spawnOpts);
+            'build',
+            '--pull',
+            '--build-arg', `ORIGINAL_IMAGE=${registry}/${tag}`,
+            '--tag', `${registry}/${newTag}`,
+            '-f', `${patchPath}/${patchConfig.dockerFile || 'Dockerfile'}`,
+            patchPath
+        ], spawnOpts);
 
         // Push update
         await asyncUtils.spawn('docker', ['push', `${registry}/${newTag}`], spawnOpts);
     });
+
+    // If config says to delete any untagged images mentioned in the patch, do so.
+    if (patchConfig.deleteUntaggedImages && patchConfig.imageIds) {
+        await deleteUntaggedImages(patchConfig.imageIds, registry);
+    }
 
     console.log('\n(*) Done!')
 }
@@ -87,6 +71,134 @@ function updateVersionTag(fullTag, registryPath) {
     return tag;
 }
 
+async function deleteUnpatchedImages(patchPath, registry) {
+    patchPath = path.resolve(patchPath);
+    const patchConfig = await getPatchConfig(patchPath);
+    if (!patchConfig.imageIds) {
+        console.log('(!) Patch does not include image IDs.  Nothing to do.');
+        return;
+    }
+    return await deleteUntaggedImages(patchConfig.imageIds, registry);
+}
+
+async function deleteUntaggedImages(imageIds, registry) {
+
+    console.log(`(*) Deleting untagged images...`);
+    // ACR registry name is the registry minus .azurecr.io
+    const registryName = registry.replace(/\..*/, '');
+
+    const manifests = await getImageManifests(registryName, imageIds);
+
+    console.log(`(*) Manifests to delete: ${JSON.stringify(manifests, undefined, 4)}`);
+
+    const spawnOpts = { stdio: 'inherit', shell: true };
+    await asyncUtils.forEach(manifests, async (manifest) => {
+        if (manifest.tags.length > 0) {
+            console.log(`(!) Skipping ${manifest.digest} because it has tags: ${manifest.tags}`);
+            return;
+        }
+        const fullImageId = `${manifest.repository}@${manifest.digest}`;
+        console.log(`(*) Deleting ${fullImageId}...`);
+        // Pull and build patched tag
+        await asyncUtils.spawn('az', [
+            'acr',
+            'repository',
+            'delete',
+            '--yes',
+            '--name', registryName,
+            '--image', fullImageId
+        ], spawnOpts);
+    });
+
+    console.log('\n(*) Done deleting manifests!')
+}
+
+async function getImageManifests(registryName, imageIds) {
+    let manifests = [];
+
+    // Get list of repositories
+    console.log(`(*) Getting repository list for ACR "${registryName}"...`)
+    const repositoryListOutput = await asyncUtils.spawn('az',
+        ['acr', 'repository', 'list', '--name', registryName],
+        { shell: true, stdio: 'pipe' });
+    const repositoryList = JSON.parse(repositoryListOutput);
+
+    // Query each repository for images, then add any tags found to the list
+    const query = imageIds.reduce((prev, current) => {
+        return prev ? `${prev} || digest=='${current}'` : `"[?digest=='${current}'`;
+    }, null) + '] | []"';
+    await asyncUtils.forEach(repositoryList, async (repository) => {
+        console.log(`(*) Getting manifests from "${repository}"...`);
+        const registryManifestListOutput = await asyncUtils.spawn('az',
+            ['acr', 'repository', 'show-manifests', '--name', registryName, '--repository', repository, "--query", query],
+            { shell: true, stdio: 'pipe' });
+        let registryManifestList = JSON.parse(registryManifestListOutput);
+        registryManifestList = registryManifestList.map((manifest) => {
+            manifest.repository = repository;
+            return manifest;
+        });
+        manifests = manifests.concat(registryManifestList);
+    });
+
+    // Scan for duplicate hashes and error if detected - indicates same hash 
+    // is in multiple repos, so update would be ambiguous. Unlikely, but it 
+    // is important to not accidentally update/delete the wrong image.
+    manifests.forEach((manifest) => {
+        const manifestsWithDigest = manifests.filter((manifestToCheck) => manifestToCheck.digest === manifest.digest)
+        if (manifestsWithDigest.length > 1) {
+            throw `Duplicate manifest detected for ${manifest.digest}. Duplicates: ${JSON.stringify(manifestsWithDigest, undefined, 4)}`
+        }
+    });
+
+    return manifests;
+}
+
+async function getPatchConfig(patchPath) {
+    const patchConfigFilePath = path.resolve(patchPath, 'patch.json');
+    if (!await asyncUtils.exists(patchConfigFilePath)) {
+        throw (`No patch.json found at ${patchConfigFilePath}`);
+    }
+    const patchConfig = await jsonc.read(patchConfigFilePath);
+
+    if (typeof patchConfig.bumpVersion === 'undefined') {
+        patchConfig.bumpVersion = true;
+    }
+    if (typeof patchConfig.deleteUntaggedImages === 'undefined') {
+        patchConfig.deleteUntaggedImages = false;
+    }
+
+    return patchConfig;
+
+}
+
+async function patchAll(registry, registryPath) {
+    const patchRoot = path.resolve(__dirname, '..', 'patch');
+    const patchStatusFilePath = path.join(patchRoot, 'status.json');
+    const patchStatus = await asyncUtils.exists(patchStatusFilePath) ? await jsonc.read(patchStatusFilePath) : { complete: {}, failed: {} }
+    patchStatus.failed = {};
+    const patchList = await asyncUtils.readdir(patchRoot, { withFileTypes: true });
+    await asyncUtils.forEach(patchList, async (patchEntry) => {
+        if (patchStatus.complete[patchEntry.name]) {
+            console.log(`(*) Patch ${patchEntry.name} already complete.`);
+            return;
+        }
+        if (patchEntry.isDirectory()) {
+            try {
+                await patch(path.join(patchRoot, patchEntry.name), registry, registryPath);
+                patchStatus.complete[patchEntry.name] = true;
+            } catch (ex) {
+                console.log(`(!) Patch ${patchEntry.name} failed - ${ex}.`);
+                patchStatus.failed[patchEntry.name] = JSON.stringify(ex, undefined, 4);
+            }
+        }
+    });
+
+    // Write status file for next time
+    await asyncUtils.writeFile(patchStatusFilePath, JSON.stringify(patchStatus, undefined, 4))
+}
+
 module.exports = {
-    patch: patch
+    patchAll: patchAll,
+    patch: patch,
+    deleteUnpatchedImages: deleteUnpatchedImages
 }
