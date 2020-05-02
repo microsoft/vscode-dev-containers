46cf16d172e8398875ba4b11e3ad2178454cd1f6
Chuck Lantz <clantz@microsoft.com>
2020-05-02 09:46:56 -0700

    Add support for variant specific tags

--- build/src/utils/config.js
+++ build/src/utils/config.js
@@ -136,19 +136,34 @@ function getTagsForVersion(definitionId, version, registry, registryPath, varian
     if (typeof config.definitionBuildSettings[definitionId] === 'undefined') {
         return null;
     }
+    
     // Use the first variant if none passed in, unless there isn't one
     if (!variant) {
         const variants = getVariants(definitionId);
         variant = variants ? variants[0] : 'NOVARIANT';
     }
-    return config.definitionBuildSettings[definitionId].tags.reduce((list, tag) => {
+    let tags  = config.definitionBuildSettings[definitionId].tags;
+    // See if there are any variant specific tags
+    const variantTags = config.definitionBuildSettings[definitionId].variantTags;
+    if(variantTags) {
+        // ${VARIANT} or $VARIANT may be passed in as a way to do lookups. Add all in this case.
+        if(['${VARIANT}', '$VARIANT'].indexOf(variant) > -1 ) {
+            for(let variantEntry in variantTags) {
+                tags = tags.concat(variantTags[variantEntry]);
+            }
+        } else {
+            tags = tags.concat(variantTags[variant]);
+        }
+    }
+
+    return tags.reduce((list, tag) => {
         // One of the tags that needs to be supported is one where there is no version, but there
         // are other attributes. For example, python:3 in addition to python:0.35.0-3. So, a version
         // of '' is allowed. However, there are also instances that are just the version, so in 
         // these cases latest would be used instead. However, latest is passed in separately.
         let baseTag = tag.replace('${VERSION}', version)
             .replace(':-', ':')
-            .replace('${VARIANT}', variant || 'NOVARIANT')
+            .replace(/\$\{?VARIANT\}?/, variant || 'NOVARIANT')
             .replace('-NOVARIANT', '');
         if (baseTag.charAt(baseTag.length - 1) !== ':') {
             list.push(`${registry}/${registryPath}/${baseTag}`);
