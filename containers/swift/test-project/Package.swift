c9a2f204d96d3cd9a538d5763dd88ab1edc67c85
Chuck Lantz <clantz@microsoft.com>
2020-07-12 14:48:16 +0000

    Update swift definition

--- containers/swift/test-project/Package.swift
+++ containers/swift/test-project/Package.swift
@@ -1,14 +1,14 @@
+// swift-tools-version:4.0
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "helloworld",
+    name: "HelloWorld",
     targets: [
-        Target(name:"helloworld")
+        .target(name:"HelloWorld")
     ]
 )
