a8285173e8f705136159a0c6116828cd2149fd03
Edmundo Gonzalez <51725820+edgonmsft@users.noreply.github.com>
2020-07-13 19:42:32 +0000

    Update oryx image and symlink to newest version.

--- containers/codespaces-linux/.devcontainer/symlinkDotNetCore.sh
+++ containers/codespaces-linux/.devcontainer/symlinkDotNetCore.sh
@@ -36,12 +36,12 @@ function createLinks() {
     done
 }
 
-createLinks "3.1.202" "3.1.4"
+createLinks "3.1.301" "3.1.5"
 echo
 createLinks "3.0.103" "3.0.3"
 echo
 createLinks "2.2.402" "2.2.7"
 echo
-createLinks "2.1.806" "2.1.17"
+createLinks "2.1.807" "2.1.19"
 echo
 createLinks "1.1.14" "1.1.13"
