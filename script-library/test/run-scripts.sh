cb5f4bcdf101262b67cb400eb7900890a1e24ce9
Chuck Lantz <clantz@microsoft.com>
2020-08-10 15:18:31 -0700

    Add Azure CLI and Terraform scripts

--- script-library/test/run-scripts.sh
+++ script-library/test/run-scripts.sh
@@ -11,6 +11,8 @@ MAVEN_VERSION=${8:-"3.6.3"}
 MAVEN_DOWNLOAD_SHA=${9:-"c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0"}
 GRADLE_VERSION=${10:-"5.4.1"}
 GRADLE_DOWNLOAD_SHA=${11:-"7bdbad1e4f54f13c8a78abc00c26d44dd8709d4aedb704d913fb1bb78ac025dc"}
+TERRAFORM_VERSION=${12:-"0.12.16"}
+TFLINT_VERSION=${13:-"0.8.2"}
 
 runScript()
 {
@@ -34,6 +36,7 @@ if [ "${DISTRO}" = "debian" ]; then
     runScript ${SCRIPT_DIR}/node-${DISTRO}.sh "/usr/local/share/nvm ${NODE_VERSION} ${USERNAME}"
     runScript ${SCRIPT_DIR}/maven-${DISTRO}.sh "/usr/local/share/maven ${USERNAME} ${MAVEN_DOWNLOAD_SHA}" "${MAVEN_VERSION}"
     runScript ${SCRIPT_DIR}/gradle-${DISTRO}.sh "/usr/local/share/gradle ${USERNAME} ${GRADLE_DOWNLOAD_SHA}" "${GRADLE_VERSION}"
+    runScript ${SCRIPT_DIR}/terraform-${DISTRO}.sh "${TFLINT_VERSION}" "${TERRAFORM_VERSION}"
 fi
 
 # Run Docker script
