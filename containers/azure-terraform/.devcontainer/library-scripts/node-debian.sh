6ef0f4fb7e4cae692f9300eced6a061e6ddce017
Chuck Lantz <clantz@microsoft.com>
2020-08-15 19:54:14 -0700

    Update other definitions that use Azure CLI

--- containers/azure-terraform/.devcontainer/library-scripts/node-debian.sh
+++ containers/azure-terraform/.devcontainer/library-scripts/node-debian.sh
@@ -13,7 +13,7 @@ USERNAME=${3:-"vscode"}
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run a root. Use sudo, su, or add "USER root" to\nyour Dockerfile before running this script.'
+    echo -e 'Script must be run a root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
