e0d95845b93ccb46ac211e05f453a030317bbec6
Chuck Lantz <clantz@microsoft.com>
2020-08-21 16:13:10 -0700

    Update Oh My Zsh! references

--- containers/codespaces-linux/.devcontainer/library-scripts/common-debian.sh
+++ containers/codespaces-linux/.devcontainer/library-scripts/common-debian.sh
@@ -166,7 +166,7 @@ fi
 if [ "${INSTALL_ZSH}" = "true" ] && [ ! -d "/root/.oh-my-zsh" ] && [ "${ZSH_ALREADY_INSTALLED}" != "true" ]; then
     apt-get-update-if-needed
     apt-get install -y zsh
-    curl -fsSLo- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash 2>&1
+    curl -fsSLo- https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash 2>&1
     echo -e "${RC_SNIPPET}\nDEFAULT_USER=\$USER\nprompt_context(){}" >> /root/.zshrc
     cp -fR /root/.oh-my-zsh /etc/skel
     cp -f /root/.zshrc /etc/skel
