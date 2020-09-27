03a5117eeb03c074a024b077928d604005036b8d
LavaAdventures <69821200+LavaAdventures@users.noreply.github.com>
2020-09-27 12:56:24 -0400

    File path and file name info
     *  File: push.js
     *  File Path: vscode-dev-containers/build/src/push.js

--- build/src/push.js
+++ build/src/push.js
@@ -1,6 +1,8 @@
 /*--------------------------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *  File: push.js
+ *  File Path: vscode-dev-containers/build/src/push.js
  *-------------------------------------------------------------------------------------------------------------*/
 
 const path = require('path');
