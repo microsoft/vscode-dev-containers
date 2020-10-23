cc4f717310a51caf753376f37c32faa3e8a6a926
Chuck Lantz <clantz@microsoft.com>
2020-10-23 22:19:23 +0000

    Multi-parent bucket fix

--- build/src/utils/config.js
+++ build/src/utils/config.js
@@ -280,7 +280,7 @@ function getSortedDefinitionBuildList(page, pageTotal, definitionsToSkip) {
                 if (parentId) {
                     // if multi-parent, merge the buckets
                     if (typeof parentId !== 'string') {
-                        parentId = createMultiPartentBuckets(parentId, parentBuckets, dupeBuckets);
+                        parentId = createMultiParentBucket(parentId, parentBuckets, dupeBuckets);
                     }
                     bucketDefinition(definitionId, parentId, parentBuckets);
                 } else {
@@ -360,27 +360,26 @@ function getSortedDefinitionBuildList(page, pageTotal, definitionsToSkip) {
 }
 
 // Handle multi-parent definitions
-function createMultiPartentBuckets(parents, parentBuckets, dupeBuckets) {
-    const parentId = parents[Object.keys(parents)[0]];
-    // Merge any other buckets that exist
-    let firstParentBucket = parentBuckets[parentId] || [parentId];
-    for (let currentParentId in parents) {
+function createMultiParentBucket(variantParentMap, parentBuckets, dupeBuckets) {
+    // Get parent of first variant
+    const parentId = variantParentMap[Object.keys(variantParentMap)[0]];
+    const firstParentBucket =  parentBuckets[parentId] || [parentId];
+    // Merge other parent buckets into the first parent
+    for (let currentVariant in variantParentMap) {
+        const currentParentId = variantParentMap[currentVariant];
         if (currentParentId !== parentId) {
             const currentParentBucket = parentBuckets[currentParentId];
-            if (currentParentBucket) {
-                firstParentBucket = firstParentBucket.concat(currentParentBucket);
+            // Merge buckets if not already merged
+            if (currentParentBucket && dupeBuckets.indexOf(currentParentId) < 0) {
+                currentParentBucket.forEach((current) => firstParentBucket.push(current));
+            } else if (firstParentBucket.indexOf(currentParentId)<0) {
+                firstParentBucket.push(currentParentId);
             }
             dupeBuckets.push(currentParentId);
+            parentBuckets[currentParentId]=firstParentBucket;
         }
     }
-
-    // Point everything to the same array
-    for (let currentParentId in parents) {
-        if (firstParentBucket.indexOf(currentParentId) < 0) {
-            firstParentBucket.push(currentParentId);
-        }
-        parentBuckets[currentParentId] = firstParentBucket;
-    }
+    parentBuckets[parentId] = firstParentBucket;
     return parentId;
 }
 
