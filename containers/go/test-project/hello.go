65cc28091697754899ec97f90dedef19590efc4d
Tom Payne <twpayne@gmail.com>
2020-12-23 17:07:32 +0100

    Format Go code with gofmt

--- containers/go/test-project/hello.go
+++ containers/go/test-project/hello.go
@@ -2,9 +2,11 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
- 
+
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("Hello remote world!")
+	fmt.Println("Hello remote world!")
 }
