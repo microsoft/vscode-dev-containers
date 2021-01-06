b6c53bd7a1ba22631019175251928f99eab723cb
Josh Spicer <jospicer@microsoft.com>
2021-01-06 12:20:46 -0500

    add sudoIfs

--- containers/docker-in-docker/.devcontainer/library-scripts/docker-in-docker-debian.sh
+++ containers/docker-in-docker/.devcontainer/library-scripts/docker-in-docker-debian.sh
@@ -134,26 +134,26 @@ set -e
 
 export container=docker
 
-if [ -d /sys/kernel/security ] && ! mountpoint -q /sys/kernel/security; then
-	mount -t securityfs none /sys/kernel/security || {
+if [ -d /sys/kernel/security ] && ! sudoIf mountpoint -q /sys/kernel/security; then
+	sudoIf mount -t securityfs none /sys/kernel/security || {
 		echo >&2 'Could not mount /sys/kernel/security.'
 		echo >&2 'AppArmor detection and --privileged mode might break.'
 	}
 fi
 
 # Mount /tmp (conditionally)
-if ! mountpoint -q /tmp; then
-	mount -t tmpfs none /tmp
+if ! sudoIf mountpoint -q /tmp; then
+	sudoIf mount -t tmpfs none /tmp
 fi
 
 # cgroup v2: enable nesting
 if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
 	# move the init process (PID 1) from the root group to the /init group,
 	# otherwise writing subtree_control fails with EBUSY.
-	mkdir -p /sys/fs/cgroup/init
-	echo 1 > /sys/fs/cgroup/init/cgroup.procs
+	sudoIf mkdir -p /sys/fs/cgroup/init
+	sudoIf echo 1 > /sys/fs/cgroup/init/cgroup.procs
 	# enable controllers
-	sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
+	sudoIf sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
 		> /sys/fs/cgroup/cgroup.subtree_control
 fi
 ## Dind wrapper over.
