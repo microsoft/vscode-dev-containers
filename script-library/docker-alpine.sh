d90e8ad7e028f4e09c694748f93f36bd3f0680fa
John Sanders <jws121295@gmail.com>
2021-01-15 19:00:48 -0500

    remove moby cli
    I don't believe alpine has a moby-cli package

--- script-library/docker-alpine.sh
+++ script-library/docker-alpine.sh
@@ -12,7 +12,6 @@ ENABLE_NONROOT_DOCKER=${1:-"true"}
 SOURCE_SOCKET=${2:-"/var/run/docker-host.sock"}
 TARGET_SOCKET=${3:-"/var/run/docker.sock"}
 USERNAME=${4:-"automatic"}
-USE_MOBY=${5:-"true"}
 
 set -e
 
@@ -58,23 +57,11 @@ if ! dpkg -s apt-transport-https curl ca-certificates lsb-release > /dev/null 2>
     apt-get -y install --no-install-recommends apt-transport-https curl ca-certificates lsb-release gnupg2 
 fi
 
-# Install Docker / Moby CLI if not already installed
+# Install Docker CLI if not already installed
 if type docker > /dev/null 2>&1; then
-    echo "Docker / Moby CLI already installed."
+    echo "Docker CLI already installed."
 else
-    if [ "${USE_MOBY}" = "true" ]; then
-        DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-        CODENAME=$(lsb_release -cs)
-        curl -s https://packages.microsoft.com/keys/microsoft.asc | (OUT=$(apt-key add - 2>&1) || echo $OUT)
-        echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-${DISTRO}-${CODENAME}-prod ${CODENAME} main" > /etc/apt/sources.list.d/microsoft.list
-        apt-get update
-        apt-get -y install --no-install-recommends moby-cli
-    else
-        curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | (OUT=$(apt-key add - 2>&1) || echo $OUT)
-        echo "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
-        apt-get update
-        apt-get -y install --no-install-recommends docker-ce-cli
-    fi
+    apk add docker-cli
 fi
 
 # Install Docker Compose if not already installed 
