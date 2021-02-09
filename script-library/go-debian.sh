e7a095ae4373edd9cebc66b41cc82a850c024e37
Hana <hyangah@gmail.com>
2021-02-09 12:21:14 -0500

    go-debian.sh: remove gocode-gomod and redundant golangci-lint

--- script-library/go-debian.sh
+++ script-library/go-debian.sh
@@ -86,11 +86,11 @@ fi
 
 # Install Go tools that are isImportant && !replacedByGopls based on
 # https://github.com/golang/vscode-go/blob/0c6dce4a96978f61b022892c1376fe3a00c27677/src/goTools.ts#L188
-GO_TOOLS_WITH_MODULES="\
+# exception: golangci-lint is installed using their install script below.
+GO_TOOLS="\
     golang.org/x/tools/gopls \
     honnef.co/go/tools/... \
     golang.org/x/lint/golint \
-    github.com/golangci/golangci-lint/cmd/golangci-lint \
     github.com/mgechev/revive \
     github.com/uudashr/gopkgs/v2/cmd/gopkgs \
     github.com/ramya-rao-a/go-outline \
@@ -105,19 +105,13 @@ if [ "${INSTALL_GO_TOOLS}" = "true" ]; then
 
     # Go tools w/module support
     export GO111MODULE=on
-    (echo "${GO_TOOLS_WITH_MODULES}" | xargs -n 1 go get -v )2>&1
-
-    # gocode-gomod
-    export GO111MODULE=auto
-    go get -v -d github.com/stamblerre/gocode 2>&1
-    go build -o gocode-gomod github.com/stamblerre/gocode
+    (echo "${GO_TOOLS}" | xargs -n 1 go get -v )2>&1
 
     # golangci-lint
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ${TARGET_GOPATH}/bin 2>&1
 
     # Move Go tools into path and clean up
     mv /tmp/gotools/bin/* ${TARGET_GOPATH}/bin/
-    mv gocode-gomod ${TARGET_GOPATH}/bin/
     rm -rf /tmp/gotools
     chown -R ${USERNAME} "${TARGET_GOPATH}"
 fi
