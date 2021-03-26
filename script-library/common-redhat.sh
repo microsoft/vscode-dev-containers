056c19d69ba6bad38d3714b3cc805f5f548c5824
Chuck Lantz <clantz@microsoft.com>
2021-03-26 23:17:23 +0000

    Add dynamic first run location

--- script-library/common-redhat.sh
+++ script-library/common-redhat.sh
@@ -150,8 +150,12 @@ if [ -z "${USER}" ]; then export USER=$(whoami); fi
 if [[ "${PATH}" != *"$HOME/.local/bin"* ]]; then export PATH="${PATH}:$HOME/.local/bin"; fi
 
 # Display optional first run image specific notice if configured and terminal is interactive
-if [ -t 1 ] && [ -f "/usr/local/etc/vscode-dev-containers/first-run-notice.txt" ] && [ ! -f "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed" ]; then
-    cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+if [ -t 1 ] && [ ! -f "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed" ]; then
+    if [ -f "/usr/local/etc/vscode-dev-containers/first-run-notice.txt" ]; then
+        cat "/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+    elif [ -f "/workspaces/.codespaces/shared/first-run-notice.txt" ]; then
+        cat "/workspaces/.codespaces/shared/first-run-notice.txt"
+    fi
     mkdir -p $HOME/.config/vscode-dev-containers
     # Mark first run notice as displayed after 10s to avoid problems with fast terminal refreshes hiding it
     (sleep 10s; touch "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed") & >/dev/null 2>&1
