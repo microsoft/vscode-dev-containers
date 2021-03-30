b15e801495d105c21344bba68bcc57f02f5d8720
Chuck Lantz <clantz@microsoft.com>
2021-03-30 14:31:40 +0000

    Avoid adding test project lock files to repo (CI Ignore)

--- containers/typescript-node/test-project/test.sh
+++ containers/typescript-node/test-project/test.sh
@@ -16,7 +16,7 @@ check "typescript" npm run compile
 check "test-project" npm run test
 check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 8"
 check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
-sudo rm -rf node_modules out
+sudo rm -rf node_modules out yarn.lock package-lock.json
 
 # Report result
 reportResults
