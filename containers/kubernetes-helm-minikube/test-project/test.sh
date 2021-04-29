b1e55f10b948f6c01ad75fec12d63a94b97af789
Chuck Lantz <clantz@microsoft.com>
2021-04-29 17:33:16 +0000

    Add kubernetes definition that uses dind and minikube

---
+++ containers/kubernetes-helm-minikube/test-project/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+source test-utils.sh vscode
+
+# Run common tests
+checkCommon
+
+# Run Docker init script
+/usr/local/share/docker-init.sh
+
+# Actual tests
+checkExtension "ms-azuretools.vscode-docker"
+checkExtension "ms-kubernetes-tools.vscode-kubernetes-tools"
+check "docker" docker ps -a
+check "kubectl" kubectl version --client
+check "helm" helm version --client
+check "minikube start" minikube start
+check "minikube remove" minikube remove
+docker image prune -a
+
+# Report result
+reportResults
+
