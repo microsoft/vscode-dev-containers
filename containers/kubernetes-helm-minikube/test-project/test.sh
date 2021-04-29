a7f65a4b59904457629ff6a0ab64a2579c1025e2
Chuck Lantz <clantz@microsoft.com>
2021-04-29 16:41:06 -0700

    Create DinD flavor of kubernetes definition, add volume mounts to DinD examples (#842)

--- containers/kubernetes-helm-minikube/test-project/test.sh
+++ containers/kubernetes-helm-minikube/test-project/test.sh
@@ -6,22 +6,18 @@ source test-utils.sh vscode
 # Run common tests
 checkCommon
 
-# Execute .bashrc with the SYNC_LOCALHOST_KUBECONFIG set
-export SYNC_LOCALHOST_KUBECONFIG=true 
-exec bash
-
 # Run Docker init script
 /usr/local/share/docker-init.sh
 
 # Actual tests
 checkExtension "ms-azuretools.vscode-docker"
 checkExtension "ms-kubernetes-tools.vscode-kubernetes-tools"
-check "docker-socket" ls -l /var/run/docker.sock 
 check "docker" docker ps -a
-check "kube-config-mount" ls -l /usr/local/share/kube-localhost
-check "kube-config" ls -l "$HOME/.kube"
 check "kubectl" kubectl version --client
 check "helm" helm version --client
+check "minikube start" minikube start
+check "minikube remove" minikube delete
+docker image prune -a -f
 
 # Report result
 reportResults
