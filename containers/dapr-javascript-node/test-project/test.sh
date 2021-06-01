45adfa20c2bb8bb586d53cb4fe6bccf2b0bbac8f
Chuck Lantz <clantz@microsoft.com>
2021-06-01 21:13:24 +0000

    Regression test fixes (CI Ignore)

--- containers/dapr-javascript-node/test-project/test.sh
+++ containers/dapr-javascript-node/test-project/test.sh
@@ -3,20 +3,26 @@ cd $(dirname "$0")
 
 source test-utils.sh node
 
+# Remote - Containers does not auto-sync UID/GID for Docker Compose,
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
 # Run common tests
 checkCommon
 
 # Definition specific tests
 checkExtension "dbaeumer.vscode-eslint"
 check "node" node --version
+sudo rm -f yarn.lock 
 check "yarn" yarn install
+sudo rm -f package-lock.json
 check "npm" npm install
 check "eslint" eslint src/server.ts
 check "typescript" npm run compile
 check "test-project" npm run test
 check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 8"
 check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
-sudo rm -rf node_modules yarn.lock package-lock.json
+sudo rm -rf node_modules
 
 # Report result
 reportResults
--- containers/javascript-node-azurite/test-project/test.sh
+++ containers/javascript-node-azurite/test-project/test.sh
@@ -3,13 +3,19 @@ cd $(dirname "$0")
 
 source test-utils.sh node
 
+# Remote - Containers does not auto-sync UID/GID for Docker Compose,
+# so make sure test project prvs match the non-root user in the container.
+fixTestProjectFolderPrivs
+
 # Run common tests
 checkCommon
 
 # Definition specific tests
 checkExtension "dbaeumer.vscode-eslint"
 check "node" node --version
+sudo rm -f yarn.lock
 check "yarn" yarn install
+sudo rm -f package-lock.json
 check "npm" npm install
 check "eslint" eslint japaFile.js
 check "test-project" npm run test
