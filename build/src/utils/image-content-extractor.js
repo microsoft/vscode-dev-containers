c00c37321c717566bd43b51d23fb5e9f538f1ddf
Chuck Lantz <clantz@microsoft.com>
2021-06-02 19:14:28 +0000

    Handle global npm package that is a sub-dependency.

--- build/src/utils/image-content-extractor.js
+++ build/src/utils/image-content-extractor.js
@@ -220,11 +220,28 @@ async function getNpmGlobalPackageInfo(imageTagOrContainerName, packageList) {
 
     console.log(`(*) Gathering information about globally installed npm packages...`);
 
-    const npmOutputRaw = await getCommandOutputFromContainer(imageTagOrContainerName, "bash -l -c 'set -e && npm ls --global --depth 0 --json' 2>/dev/null");
+    const packageListString = packageList.reduce((prev, current) => prev + ' ' + current, '');
+    const npmOutputRaw = await getCommandOutputFromContainer(imageTagOrContainerName, `bash -l -c 'set -e && npm ls --global --depth 1 --json ${packageListString}' 2>/dev/null`);
     const npmOutput = JSON.parse(npmOutputRaw);
 
     return packageList.map((package) => {
-        const packageJson =  npmOutput.dependencies[package] || {};
+        let packageJson =  npmOutput.dependencies[package];
+        if (!packageJson) {
+            // Possible desired package is referenced by another top level package, so check dependencies too.
+            // E.g. tslint-to-eslint-config can cause typescript to not appear at top level in npm ls
+            for (let packageInNpmOutput in npmOutput.dependencies) {
+                const packageDependencies = npmOutput.dependencies[packageInNpmOutput].dependencies;
+                if(packageDependencies) {
+                    packageJson = packageDependencies[package];
+                    if(packageJson) {
+                        break;
+                    }    
+                }
+            }
+        }
+        if(!packageJson || !packageJson.version) {
+            throw new Error(`Unable to parse version for ${package} from npm ls output: ${npmOutputRaw}`);
+        }
         return {
             name: package,
             version:packageJson.version
