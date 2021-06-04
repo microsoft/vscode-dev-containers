1d907fca539abb39d17f184c8a92afbcecc0bb50
Chuck Lantz <clantz@microsoft.com>
2021-06-04 02:38:56 +0000

    Remove login shell PATH workaround given this is supported in product

--- script-library/common-redhat.sh
+++ script-library/common-redhat.sh
@@ -24,11 +24,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
-rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
-chmod +x /etc/profile.d/00-restore-env.sh
-
 # If in automatic mode, determine if a user already exists, if not use vscode
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
--- script-library/python-debian.sh
+++ script-library/python-debian.sh
@@ -23,11 +23,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
-rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
-chmod +x /etc/profile.d/00-restore-env.sh
-
 # Determine the appropriate non-root user
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""
