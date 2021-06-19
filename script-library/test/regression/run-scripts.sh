16ba2cea2cb48594e9f1194a259708a1faba67e3
Chuck Lantz <clantz@microsoft.com>
2021-06-19 16:16:16 +0000

    Support single script regression test

--- script-library/test/regression/run-scripts.sh
+++ script-library/test/regression/run-scripts.sh
@@ -6,12 +6,17 @@ USE_DEFAULTS=${3:-"true"}
 USERNAME=${4:-"vscode"}
 RUN_COMMON_SCRIPT=${5:-"true"}
 UPGRADE_PACKAGES=${6:-"true"}
+RUN_ONE=${7:-"false"} # false or script name
 
 set -e
 
 runScript()
 {
-    SCRIPT=$1
+    SCRIPT_NAME=$1
+    if [ "${RUN_ONE}" != "false" ] && [ "${RUN_ONE}" != "common" ] && [ "${SCRIPT_NAME}" != "${RUN_ONE}" ]; then
+        return
+    fi
+    SCRIPT=${SCRIPT_DIR}/${SCRIPT_NAME}-${DISTRO}.sh
     ARGS=$2
     REQUIRED_PREFIX_ARGS=${3:-""}
     echo "**** Testing $SCRIPT ****"
@@ -26,36 +31,39 @@ runScript()
 }
 
 if [ "${RUN_COMMON_SCRIPT}" = "true" ]; then
-    runScript ${SCRIPT_DIR}/common-${DISTRO}.sh "true ${USERNAME} 1000 1000 ${UPGRADE_PACKAGES}"
+    runScript common "true ${USERNAME} 1000 1000 ${UPGRADE_PACKAGES}"
 fi
 
 if [ "${DISTRO}" = "debian" ]; then
-    runScript ${SCRIPT_DIR}/azcli-${DISTRO}.sh
-    runScript ${SCRIPT_DIR}/fish-${DISTRO}.sh "false ${USERNAME}"
-    runScript ${SCRIPT_DIR}/git-from-src-${DISTRO}.sh "2.26.2"
-    runScript ${SCRIPT_DIR}/git-lfs-${DISTRO}.sh
-    runScript ${SCRIPT_DIR}/github-${DISTRO}.sh
-    runScript ${SCRIPT_DIR}/go-${DISTRO}.sh "1.14 /opt/go /go ${USERNAME} false"
-    runScript ${SCRIPT_DIR}/gradle-${DISTRO}.sh "4.4 /usr/local/sdkman1 ${USERNAME} false"
-    runScript ${SCRIPT_DIR}/homebrew-${DISTRO}.sh "${USERNAME} false true /home/${USERNAME}/linuxbrew"
-    runScript ${SCRIPT_DIR}/java-${DISTRO}.sh "13.0.2.j9-adpt /usr/local/sdkman2 ${USERNAME} false"
-    runScript ${SCRIPT_DIR}/kubectl-helm-${DISTRO}.sh
-    runScript ${SCRIPT_DIR}/maven-${DISTRO}.sh "3.6.3 /usr/local/sdkman3 ${USERNAME} false" 
-    runScript ${SCRIPT_DIR}/node-${DISTRO}.sh "/usr/local/share/nvm 10 ${USERNAME}"
-    runScript ${SCRIPT_DIR}/powershell-${DISTRO}.sh
-    runScript ${SCRIPT_DIR}/python-${DISTRO}.sh "3.4.10 /opt/python ${USERNAME} false false"
-    runScript ${SCRIPT_DIR}/ruby-${DISTRO}.sh "${USERNAME} false" "2.5.8"
-    runScript ${SCRIPT_DIR}/rust-${DISTRO}.sh "/opt/rust/cargo /opt/rust/rustup ${USERNAME} false"
-    runScript ${SCRIPT_DIR}/terraform-${DISTRO}.sh "0.12.16" "0.8.2"
-    runScript ${SCRIPT_DIR}/sshd-${DISTRO}.sh "2223 ${USERNAME} true random"
-    runScript ${SCRIPT_DIR}/desktop-lite-${DISTRO}.sh "${USERNAME} changeme false"
+    runScript azcli
+    runScript fish "false ${USERNAME}"
+    runScript git-from-src "2.26.2"
+    runScript git-lfs
+    runScript github
+    runScript go "1.14 /opt/go /go ${USERNAME} false"
+    runScript gradle "4.4 /usr/local/sdkman1 ${USERNAME} false"
+    runScript homebrew "${USERNAME} false true /home/${USERNAME}/linuxbrew"
+    runScript java "13.0.2.j9-adpt /usr/local/sdkman2 ${USERNAME} false"
+    runScript kubectl-helm "latest latest latest"
+    runScript maven "3.6.3 /usr/local/sdkman3 ${USERNAME} false" 
+    runScript node "/usr/local/share/nvm 10 ${USERNAME}"
+    runScript powershell
+    runScript python "3.4.10 /opt/python ${USERNAME} false false"
+    runScript ruby "${USERNAME} false" "2.5.8"
+    runScript rust "/opt/rust/cargo /opt/rust/rustup ${USERNAME} false"
+    runScript terraform "0.12.16" "0.8.2"
+    runScript sshd "2223 ${USERNAME} true random"
+    runScript desktop-lite "${USERNAME} changeme false"
+    runSciprt docker-in-docker "false ${USERNAME} false"
 fi
 
-# Run Docker script
-if  [ "${DISTRO}" != "alpine" ]; then
-    runScript /tmp/docker-${DISTRO}.sh "true /var/run/docker-host.sock /var/run/docker.sock ${USERNAME}"
-else
-    echo '#!/bin/bash\n"$@"' | tee /usr/local/share/docker-init.sh
+if [ "${DISTRO}" != "alpine" ] && [ "${RUN_ONE}" != "docker-in-docker" ]; then
+    rm -f /usr/local/share/docker-init.sh
+    runScript docker "true /var/run/docker-host.sock /var/run/docker.sock ${USERNAME}"
+fi
+
+if [ "${DISTRO}" = "alpine" ]; then
+    echo '#!/bin/bash\n"$@"' > /usr/local/share/docker-init.sh
     chown ${USERNAME} /usr/local/share/docker-init.sh
     chmod +x /usr/local/share/docker-init.sh
 fi
--- script-library/test/regression/test.sh
+++ script-library/test/regression/test.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 IMAGE_TO_TEST="${1:-debian}"
+RUN_ONE="${2:-false}" # false or script name
+USE_DEFAULTS="${3:-true}"
+RUN_COMMON_SCRIPT="${4:-true}"
 
-if [ "$IMAGE_TO_TEST" = "centos" ]; then
-    DISTRO="redhat"
-elif [ "${IMAGE_TO_TEST}" = "ubuntu" ]; then
+if [[ "${IMAGE_TO_TEST}" = *"ubuntu"* ]]; then
     DISTRO="debian"
+elif [[ "${IMAGE_TO_TEST}" = *"alpine"* ]]; then
+    DISTRO="alpine"
+elif [[ "$IMAGE_TO_TEST" = *"centos"* ]] || [[ "$IMAGE_TO_TEST" = *"redhat"* ]]; then
+    DISTRO="redhat"
 else
     DISTRO=$IMAGE_TO_TEST
 fi
 
 set -e
 
-cd "$(cd "$(dirname "$0")" && pwd)/../.."
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../.."
 echo -e  "🧪 Testing image $IMAGE_TO_TEST (${DISTRO}-like)..."
-docker build --build-arg DISTRO=$DISTRO --build-arg IMAGE_TO_TEST=$IMAGE_TO_TEST -f test/regression/Dockerfile .
+docker build \
+    --init \
+    --privileged \
+    --build-arg DISTRO=$DISTRO \
+    --build-arg IMAGE_TO_TEST=$IMAGE_TO_TEST \
+    --build-arg RUN_ONE=${RUN_ONE} \
+    --build-arg RUN_COMMON_SCRIPT=${RUN_COMMON_SCRIPT} \
+    --build_arg USE_DEFAULTS=${USE_DEFAULTS} \
+    -f test/regression/Dockerfile .
 echo -e "\n🎉 All tests passed!"
