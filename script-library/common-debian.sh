b93b52884d1396f26ffac05ecc04d26f359344e6
Josh Spicer <josh.spicer@microsoft.com>
2021-07-06 20:09:43 -0400

    change our bash's git prompt to make zsh closer

--- script-library/common-debian.sh
+++ script-library/common-debian.sh
@@ -291,7 +291,7 @@ __bash_prompt() {
         && [ ! -z "${GITHUB_USER}" ] && echo -n "\[\033[0;32m\]@${GITHUB_USER} " || echo -n "\[\033[0;32m\]\u " \
         && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]➜" || echo -n "\[\033[0m\]➜"`'
     local gitbranch='`\
-        export BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null); \
+        export BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null); \
         if [ "${BRANCH}" = "HEAD" ]; then \
             export BRANCH=$(git describe --contains --all HEAD 2>/dev/null); \
         fi; \
