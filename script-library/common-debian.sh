01d0bc93625563321c6fadf133bf7342e6c1b8dc
Josh Spicer <josh.spicer@microsoft.com>
2021-07-08 17:36:29 +0000

    add back 'branch != '''

--- script-library/common-debian.sh
+++ script-library/common-debian.sh
@@ -292,11 +292,13 @@ __bash_prompt() {
         && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]➜" || echo -n "\[\033[0m\]➜"`'
     local gitbranch='`\
         export BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null); \
+        if [ "${BRANCH}" != "" ]; then \
             echo -n "\[\033[0;36m\](\[\033[1;31m\]${BRANCH}" \
             && if git ls-files --error-unmatch -m --directory --no-empty-directory -o --exclude-standard ":/*" > /dev/null 2>&1; then \
                     echo -n " \[\033[1;33m\]✗"; \
-            fi \
+               fi \
             && echo -n "\[\033[0;36m\]) "; `'
+        fi`'
     local lightblue='\[\033[1;34m\]'
     local removecolor='\[\033[0m\]'
     PS1="${userpart} ${lightblue}\w ${gitbranch}${removecolor}\$ "
