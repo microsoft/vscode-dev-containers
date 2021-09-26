ddfc719b9b9e502d8c0f1e54ecb6ba485a4a0fb0
Chuck Lantz <chuck_lantz@hotmail.com>
2021-09-26 16:04:20 -0700

    Improve JDK 8 handling

--- script-library/container-features/src/java-wrapper.sh
+++ script-library/container-features/src/java-wrapper.sh
@@ -9,12 +9,17 @@ USERNAME=${3:-"automatic"}
 UPDATE_RC=${4:-"true"}
 ADDITIONAL_JAVA_VERSION=11
 
-# If the user selected JDK 8, install the JDK 11 as well - update settings
+is_jdk_8="false"
 if echo "${JAVA_VERSION}" | grep -E '^8([\s\.]|$)' > /dev/null 2>&1; then
-    echo "(*) Installing secondary JDK since the Java VS Code extension requires a more recent version..."
+    is_jdk_8="true"
+fi
+
+# If the user selected JDK 8, install the JDK 11 as well since this is needed by the Java extension
+if [ "${is_jdk_8}" = "true" ]; then
+    echo "(*) Installing JDK ${ADDITIONAL_JAVA_VERSION} as Java VS Code extension requires a recent JDK..."
     /tmp/build-features/java-debian.sh "${ADDITIONAL_JAVA_VERSION}" "${SDKMAN_DIR}" "${USERNAME}" "${UPDATE_RC}"
-    jdk_11_folder=$(ls --format=single-column /usr/local/sdkman/candidates/java | grep -oE -m 1 '11\..+')
-    ln -s /usr/local/sdkman/candidates/java/${jdk_11_folder} /extension-java-home
+    jdk_11_folder="$(ls --format=single-column ${SDKMAN_DIR}/candidates/java | grep -oE -m 1 '11\..+')"
+    ln -s "${SDKMAN_DIR}/candidates/java/${jdk_11_folder}" /extension-java-home
 
     # Determine the appropriate non-root user
     username=""
@@ -29,11 +34,16 @@ if echo "${JAVA_VERSION}" | grep -E '^8([\s\.]|$)' > /dev/null 2>&1; then
         username=root
     fi
 else
-    ln -s /usr/local/sdkman/candidates/java/current /extension-java-home
+    ln -s ${SDKMAN_DIR}/candidates/java/current /extension-java-home
 fi
 
 echo "(*) Installing JDK ${JAVA_VERSION}..."
 /tmp/build-features/java-debian.sh "${JAVA_VERSION}" "${SDKMAN_DIR}" "${USERNAME}" "${UPDATE_RC}"
-
-# Create a symlink for each installed JDK version
-ls --format=single-column /usr/local/sdkman/candidates/java | xargs 
+if [ "${is_jdk_8}" = "true" ]; then
+    # Set current and default version to last SDK installed
+    jdk_full_version="$(ls --format=single-column "${SDKMAN_DIR}/candidates/java" | sort -rV | grep -oE -m 1 "${JAVA_VERSION}\\..+" )"
+    echo "(*) Setting default JDK to ${jdk_full_version}..."
+    . ${SDKMAN_DIR}/bin/sdkman-init.sh 
+    sdk use java "${jdk_full_version}"
+    sdk default java "${jdk_full_version}"
+fi
