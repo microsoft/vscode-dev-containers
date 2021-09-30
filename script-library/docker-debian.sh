49ced1ea9607f1b74c98ab10793369c4e97b08be
Chuck Lantz <clantz@microsoft.com>
2021-09-29 19:58:49 -0500

    Fix regex

--- script-library/docker-debian.sh
+++ script-library/docker-debian.sh
@@ -143,11 +143,14 @@ if [ "${CLI_VERSION}" = "latest" ] || [ "${CLI_VERSION}" = "lts" ] || [ "${CLI_V
     cli_version_suffix=""
 else    
     # Fetch a valid version from the apt-cache (eg: the Microsoft repo appends +azure, breakfix, etc...)
-    cli_version_suffix="=$(apt-cache madison ${cli_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "^(${CLI_VERSION})(\.|$|\+.*|-.*)")"
-
+    docker_version_dot_escaped="${DOCKER_VERSION//./\\.}"
+    docker_version_dot_plus_escaped="${docker_version_dot_escaped//+/\\+}"
+    docker_version_regex="^${docker_version_dot_plus_escaped}([\\.\\+ ]|$)"
+    set +e # Don't exit if finding version fails - will handle gracefully
+    cli_version_suffix="=$(apt-cache madison ${cli_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "${docker_version_regex}")"
+    set -e
     if [ -z ${cli_version_suffix} ] || [ ${cli_version_suffix} = "=" ]; then
-        echo "ERR: Parsed CLI_VERSION (${CLI_VERSION}) was not found in the apt-cache for this package+distribution combo";
-        echo "Available versions for your distribution (NOTE: pass to this script in the form -> MAJOR.MINOR.REV)"
+        echo "(!) Docker version ${DOCKER_VERSION} not found for ${ID} ${VERSION_CODENAME} ${architecture}. Available versions:"
         apt-cache madison ${cli_package_name} | awk -F"|" '{print $2}'
         exit 1
     fi
@@ -170,7 +173,7 @@ if type docker > /dev/null 2>&1; then
 else
     if [ "${USE_MOBY}" = "true" ]; then
         apt-get -y install --no-install-recommends moby-cli${cli_version_suffix} moby-buildx
-        apt-get -y install --no-install-recommends moby-compose || echo "(*) Package moby-compose (Docker Compose v2) not available for ${VERSION_CODENAME} ${architecture}. Skipping."
+        apt-get -y install --no-install-recommends moby-compose || echo "(*) Package moby-compose (Docker Compose v2) not available for ${ID} ${VERSION_CODENAME} ${architecture}. Skipping."
     else
         apt-get -y install --no-install-recommends docker-ce-cli${cli_version_suffix}
     fi
--- script-library/docker-in-docker-debian.sh
+++ script-library/docker-in-docker-debian.sh
@@ -160,11 +160,15 @@ if [ "${DOCKER_VERSION}" = "latest" ] || [ "${DOCKER_VERSION}" = "lts" ] || [ "$
     cli_version_suffix=""
 else
     # Fetch a valid version from the apt-cache (eg: the Microsoft repo appends +azure, breakfix, etc...)
-    engine_version_suffix="=$(apt-cache madison ${engine_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "^(${DOCKER_VERSION})(\.|$|\+.*|-.*)")"
-    cli_version_suffix="=$(apt-cache madison ${cli_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "^(${DOCKER_VERSION})(\.|$|\+.*|-.*)")"
-    if [ ${engine_version_suffix} = "=" ] || [ ${cli_version_suffix} = "=" ] ; then
-        echo "ERR: Provided VERSION (${DOCKER_VERSION}) was not found in the apt-cache for the CLI and/or Engine in this distribution";
-        echo "Available *engine* versions for your distribution (NOTE: pass to this script in the form -> MAJOR.MINOR.REV)"
+    docker_version_dot_escaped="${DOCKER_VERSION//./\\.}"
+    docker_version_dot_plus_escaped="${docker_version_dot_escaped//+/\\+}"
+    docker_version_regex="^${docker_version_dot_plus_escaped}([\\.\\+ ]|$)"
+    set +e # Don't exit if finding version fails - will handle gracefully
+    cli_version_suffix="=$(apt-cache madison ${cli_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "${docker_version_regex}")"
+    engine_version_suffix="=$(apt-cache madison ${engine_package_name} | awk -F"|" '{print $2}' | sed -e 's/^[ \t]*//' | grep -E -m 1 "${docker_version_regex}")"
+    set -e
+    if [ -z "${engine_version_suffix}" ] || [ "${engine_version_suffix}" = "=" ] || [ -z "${cli_version_suffix}" ] || [ "${cli_version_suffix}" = "=" ] ; then
+        echo "(!) Docker version ${DOCKER_VERSION} not found for ${ID} ${VERSION_CODENAME} ${architecture}. Available versions:"
         apt-cache madison ${engine_package_name} | awk -F"|" '{print $2}'
         exit 1
     fi
