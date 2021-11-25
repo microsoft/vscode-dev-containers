b9097b768ba0bcae66c7aa014f78e532165f3a13
twsl <45483159+twsl@users.noreply.github.com>
2021-11-25 22:57:44 +0100

    Initial commit

---
+++ containers/python-3-cuda/test-project/hello.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+import subprocess
+
+print('Hello, remote world!')
+rc = subprocess.call("nvidia-smi")
