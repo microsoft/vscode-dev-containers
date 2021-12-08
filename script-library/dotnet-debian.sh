7a2504b4732d8b0203d632665844e9b50527ba28
Kristjana Popovski <krpopovs@microsoft.com>
2021-12-08 11:03:58 -0500

    added md file for dotnet

--- script-library/dotnet-debian.sh
+++ script-library/dotnet-debian.sh
@@ -4,7 +4,7 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 #
-# Docs:
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/dotnet.md
 # Maintainer: The VS Code and Codespaces Teams
 #
 # Syntax: ./dotnet-debian.sh [dotnet version] [dotnet runtime only]
