966cbfd74ffc97e7f8ecebd2009726c8c660a283
Dan Balma <dbalma@microsoft.com>
2022-01-04 17:59:10 -0600

    cleanup empty lines

--- containers/java-postgres/test-project/src/main/java/com/mycompany/app/App.java
+++ containers/java-postgres/test-project/src/main/java/com/mycompany/app/App.java
@@ -11,9 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        
         System.out.println( "Hello Remote World!" );
         System.out.println("The XML namespace prefix is: " + XML_NS_PREFIX);
-
     }
 }
