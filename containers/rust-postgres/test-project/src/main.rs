d6d842339f3d32d7714a6b242c75c29b05f27c3a
Juan Zuluaga <juzuluag@microsoft.com>
2022-01-07 18:03:11 -0500

    Fix clippy

--- containers/rust-postgres/test-project/src/main.rs
+++ containers/rust-postgres/test-project/src/main.rs
@@ -3,9 +3,9 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
- use std::env;
- extern crate postgres;
- use postgres::{Client, NoTls};
+use std::env;
+extern crate postgres;
+use postgres::{Client, NoTls};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let name = "VS Code Remote - Containers";
@@ -14,17 +14,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let user = env::var("POSTGRES_USER")?;
     let passwd = env::var("POSTGRES_PASSWORD")?;
     let db = env::var("POSTGRES_DB")?;
-    let conn_str = format!("postgresql://{}:{}@{}/{}", user, passwd, host, db);
+    let conn_str = format!("postgresql://{}:{}@{}:5432/{}", user, passwd, host, db);
 
-    let mut conn = match Client::connect(&conn_str,NoTls) {
+    let mut conn = match Client::connect(&conn_str, NoTls) {
         Ok(conn) => conn,
-        Err(err) => return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::NotConnected,
-            format!("Connection failed {}", err),
-        ))),
+        Err(err) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::NotConnected,
+                format!("Connection failed {}", err),
+            )))
+        }
     };
     println!("Connection succeed");
     let rows = conn.execute("select * from pg_database limit 1;", &[])?;
     println!("Rows found: {}", rows);
-    return Ok(());
+    Ok(())
 }
