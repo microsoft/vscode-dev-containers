899cecabb7986f42d03c1fc4a369ef652ef58d1e
Juan Zuluaga <juzuluag@microsoft.com>
2022-01-06 19:03:13 -0500

    Ignore build dir, add main.out to run, include cpp ext pack.
    Co-authored-by: juzuluag@microsoft.com

--- containers/cpp-mariadb/test-project/test.sh
+++ containers/cpp-mariadb/test-project/test.sh
@@ -10,6 +10,7 @@ checkCommon
 checkExtension "ms-vscode.cpptools"
 checkOSPackages "command-line-tools" build-essential cmake cppcheck valgrind clang lldb llvm gdb
 check "g++"  g++ -g main.cpp -o main.out -lmariadbcpp
+check "main.out" ./main.out
 rm main.out
 mkdir -p build
 cd build
