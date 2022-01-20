bcd0dbcbb8abb3b920a7b4a60d52dbcf02237f0a
Juan Zuluaga <juzuluag@microsoft.com>
2022-01-20 08:35:50 -0500

    Add debug test, set lldb flag to disable ASLR

--- containers/rust-postgres/test-project/tests/db_test.rs
+++ containers/rust-postgres/test-project/tests/db_test.rs
@@ -3,14 +3,14 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-use std::{env, net::TcpStream, panic};
+use std::{env, net::TcpStream};
 extern crate postgres;
 use postgres::{Client, NoTls};
 
 fn getenv(name: &str) -> String {
     let val = match env::var(name) {
         Ok(val) => val,
-        Err(err) => panic!("{} {}", name, err.to_string()),
+        Err(err) => panic!("Error {} {:?}", name, err),
     };
     assert!(!val.is_empty());
     val
