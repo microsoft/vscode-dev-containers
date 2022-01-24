65bd3e627b0f544fffe01e8f7bd826a5c2663129
Dan Balma <dbalma@microsoft.com>
2022-01-24 16:16:32 -0600

    updates to match tweaks to anaconda-postgres work

---
+++ containers/python-3-miniconda-postgres/test-project/plot.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+
+#%%
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("plot.png")
+plt.show()
+
+print('Open test-project/plot.png to see the result!')
