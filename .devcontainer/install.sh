024d5c34dbc39bebaa223619802e369fde780772
Chuck Lantz <chuck_lantz@hotmail.com>
2022-03-03 18:18:49 +0000

    Fixes and tweaks

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -2,6 +2,8 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
+MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
+
 # Install VS Code for use in desktop if needed
 curl -sSL ${MICROSOFT_GPG_KEYS_URI} | gpg --dearmor > /usr/share/keyrings/microsoft-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
