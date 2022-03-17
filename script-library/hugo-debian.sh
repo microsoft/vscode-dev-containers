ecef71ca43b756224d0f8b67d7772631f7c886fc
Samruddhi Khandale <skhandale@microsoft.com>
2022-03-17 17:24:20 +0000

    fixing test

--- script-library/hugo-debian.sh
+++ script-library/hugo-debian.sh
@@ -1,4 +1,4 @@
-#!/usr/bin bash
+#!/usr/bin/env bash
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
