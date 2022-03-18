6b6868c985c392a74a7fdc73ee29baa785bd2371
Josh Spicer <josh@joshspicer.com>
2022-03-18 08:32:08 -0400

    Update script-library/docker-in-docker-debian.sh
    Co-authored-by: Chuck Lantz <chuck_lantz@hotmail.com>

--- script-library/docker-in-docker-debian.sh
+++ script-library/docker-in-docker-debian.sh
@@ -16,7 +16,7 @@ DOCKER_VERSION=${4:-"latest"} # The Docker/Moby Engine + CLI should match in ver
 MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
 DOCKER_DASH_COMPOSE_VERSION="1"
 DOCKER_IN_DOCKER_MOBY_ARCHIVE_VERSION_CODENAMES="buster bullseye bionic focal"
-DOCKER_IN_DOCKER_LICENSED_ARCHIVE_VERSION_CODENAMES="buster bullseye bionic focal hirsute"
+DOCKER_IN_DOCKER_LICENSED_ARCHIVE_VERSION_CODENAMES="buster bullseye bionic focal hirsute impish"
 
 # Default: Exit on any failure.
 set -e
