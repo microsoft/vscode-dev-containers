267a9300fa7395fb4f7ec95aa27f4042adfa39a1
Josh Spicer <josh@joshspicer.com>
2022-05-06 13:36:13 -0400

    add safe dir (#1435) (#1436)

--- build/src/utils/image-content-extractor.js
+++ build/src/utils/image-content-extractor.js
@@ -333,7 +333,7 @@ async function getGitRepositoryInfo(imageTagOrContainerName, gitRepos) {
         if (typeof repoPath === 'string') {
             console.log(`(*) Getting remote and commit for ${repoName} at ${repoPath}...`);
             // Go to the specified folder, see if the commands have already been run, if not run them and get output
-            const remoteAndCommitOutput = await getCommandOutputFromContainer(imageTagOrContainerName, `cd \\"${repoPath}\\" && if [ -f \\".git-remote-and-commit\\" ]; then cat .git-remote-and-commit; else git remote get-url origin && git log -n 1 --pretty=format:%H -- . | tee /dev/null; fi`,true);
+            const remoteAndCommitOutput = await getCommandOutputFromContainer(imageTagOrContainerName, `git config --global --add safe.directory \\"${repoPath}\\" && cd \\"${repoPath}\\" && if [ -f \\".git-remote-and-commit\\" ]; then cat .git-remote-and-commit; else git remote get-url origin && git log -n 1 --pretty=format:%H -- . | tee /dev/null; fi`,true);
             const [gitRemote, gitCommit] = remoteAndCommitOutput.split('\n');
             componentList.push({
                 name: repoName,
