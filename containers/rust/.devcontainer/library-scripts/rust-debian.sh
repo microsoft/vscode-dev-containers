5b2c4bd7799e5951cdbfb64c3397f8d649625c0b
lizelive <github@lize.live>
2022-05-16 15:04:54 -0700

    add some common tools

--- containers/rust/.devcontainer/library-scripts/rust-debian.sh
+++ containers/rust/.devcontainer/library-scripts/rust-debian.sh
@@ -184,6 +184,9 @@ fi
 echo "Installing common Rust dependencies..."
 rustup component add rls rust-analysis rust-src rustfmt clippy 2>&1
 
+echo "Installing common Rust tools..."
+sudo --user=$USERNAME cargo install bindgen cbindgen cargo-outdated cargo-audit cargo-edit cargo-diet cargo-chef
+
 # Add CARGO_HOME, RUSTUP_HOME and bin directory into bashrc/zshrc files (unless disabled)
 updaterc "$(cat << EOF
 export RUSTUP_HOME="${RUSTUP_HOME}"
