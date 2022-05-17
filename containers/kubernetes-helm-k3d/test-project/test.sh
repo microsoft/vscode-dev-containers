36f74b3e4d37369787130a45e5d59632f8075e0c
Kushal Azim Ekram <kforeverisback@gmail.com>
2022-05-17 19:02:41 +0000

    Added k3d delete test. Comment postStartCommand. Modify kubectl version to updated kubectl version --client=true

--- containers/kubernetes-helm-k3d/test-project/test.sh
+++ containers/kubernetes-helm-k3d/test-project/test.sh
@@ -10,10 +10,11 @@ checkCommon
 checkExtension "ms-azuretools.vscode-docker"
 checkExtension "ms-kubernetes-tools.vscode-kubernetes-tools"
 check "docker" docker ps -a
-check "kubectl" kubectl version --client
+check "kubectl" kubectl version --client=true --output=yaml
 check "helm" helm version --client
 check "k3d start" k3d cluster start
 check "k3d stop" k3d cluster stop
+check "k3d delete" k3d cluster delete
 docker image prune -a -f
 
 # Report result
